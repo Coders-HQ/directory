7b6a5172b4c0f5c2b9e544678e87215e5deb02ac
rashed <x.suwaidi@gmail.com>
2022-11-27 20:13:57 +0400

    update db

--- src/data.js
+++ src/data.js
@@ -62,6 +62,171 @@ const data = {
                 },
             ]
         },
+        {
+            username: "adamahsan",
+            name: "Adam Ahsan",
+            headline: "Senior at UOWD",
+            position: "Artificial Intelligence, Digital Economy and Remote Work Applications Office",
+            about: "I'm a final year computer science student pursuing my dreams at the University of Wollongong, Dubai. I have been programming since an early age and have been fascinated with technology ever since. I love to learn new things, I am very passionate about what I do and am always looking for ways to improve myself and my skillset.",
+            banner: "https://media-exp1.licdn.com/dms/image/C4D16AQF2sFMJUKEw9g/profile-displaybackgroundimage-shrink_350_1400/0/1660384843936?e=1675296000&v=beta&t=j6zI7hpl1uAGS2gib3uhzvUFMS0kOa49vMu17FQUmfQ",
+            image: "https://media-exp1.licdn.com/dms/image/C4D03AQEyick7ypba-w/profile-displayphoto-shrink_400_400/0/1624710403133?e=1675296000&v=beta&t=TcHfkMvOAxB1iVV1DYbGd6wnMItrpKIEKqety6Qn79I",
+            highlights: [
+                "Bachelor's Degree in Computer Science From University of Wollongong in Dubai",
+                'Silver Coding Ambassador',
+                '1st Place in the FutureNow Hackathon by e& enterprise (Etisalat) and Twilio',
+                '2nd Place in DedSec CTF (Inter-university CTF)'
+            ],
+            socials: [
+                {
+                    name: "LinkedIn",
+                    url: "https://www.linkedin.com/in/adamahsan/",
+                    icon: SlSocialLinkedin,
+                },
+                {
+                    name: "Github",
+                    url: "https://github.com/AdamJeddy",
+                    icon: SlSocialGithub,
+                },
+            ],
+            timeline: [
+                {
+                    date: "October 2022",
+                    title: "Volunteered for UAE Codes Day at MCC",
+                    description: "",
+                },
+                {
+                    date: "September 2022",
+                    title: "Became a Coding Ambassador",
+                    description: "Selected by CodersHQ to become a Silver Ambassador",
+                },
+            ],
+            metadata: [
+                {
+                    key: "Occupation",
+                    value: "Student",
+                },
+                {
+                    key: "Years of Experience",
+                    value: "2",
+                },
+                {
+                    key: "Academic Degree",
+                    value: "BSc in Computer Science (Big Data)",
+                },
+            ]
+        },
+        {
+            username: "naisofly",
+            name: "Naiyarah Hussain",
+            headline: "Senior Developer Relations Manager at Lightning AI | Experienced Developer Advocate & Ethical AI Enthusiast",
+            position: "Lightning AI",
+            about: "I am an experienced Developer Relations Leader & Machine Learning Engineer with a demonstrated history of working with developers from Open Source communities, enterprises, startups & universities. I'm skilled in solving problems using Machine Learning and AI, like Malware Analysis, Object & Gesture Recognition, and Natural Language to name a few. I have a strong engineering background with a BSc (Hons) in Computer Systems focused on Software Engineering from Heriot-Watt University. On the weekends, if I'm not at a meetup or hackathon, you'll find me playing basketball, diving, camping, or exploring countries with friends.",
+            banner: "https://media-exp1.licdn.com/dms/image/C4D16AQGYKDJGwlAjrw/profile-displaybackgroundimage-shrink_350_1400/0/1655486030664?e=1675296000&v=beta&t=kx0GoNLooHTTvsLd18IjEBamXSnn6OVu1DWzp_j9sX0",
+            image: "https://media-exp1.licdn.com/dms/image/C4E03AQHYC0siJz9bVw/profile-displayphoto-shrink_400_400/0/1556451682913?e=1674691200&v=beta&t=zJ3ThM-teekpDMI01mtLnXjNzvMQEnGvfJ-ulFrglUo",
+            highlights: [
+                'Moving projects from research to production with a community of 10k Ph.D. students, Researchers, and ML Engineers from academic & industry backgrounds.',
+                'Submitted two invention disclosures to be filed as patents using Machine Learning, specifically related to Natural Language Processing (System & Method to Simplify Terms & Conditions) & Computer Vision (System & Method to Detect Deepfakes).',
+                'Recently worked on Interactive Research Poster Applications, Stable Diffusion, and other Text to Image generators',
+                'Current focus areas include Real Time Machine Learning & Ethical AI'
+            ],
+            socials: [
+                {
+                    name: "LinkedIn",
+                    url: "https://www.linkedin.com/in/naiyarah/",
+                    icon: SlSocialLinkedin,
+                },
+                {
+                    name: "Twitter",
+                    url: "https://twitter.com/naisofly",
+                    icon: SlSocialTwitter,
+                },
+                {
+                    name: "Github",
+                    url: "https://github.com/naisofly",
+                    icon: SlSocialGithub,
+                },
+            ],
+            timeline: [
+                {
+                    date: "November 2022",
+                    title: "Judge at iCodeJr Code Battle",
+                    description: "Judged the iCodeJr Code Battle codebattle.tech with other CodersHQ Ambassadors",
+                },
+                {
+                    date: "October 2022",
+                    title: "Wildlife Detection Project",
+                    description: "Supported the project lead with data labelling & project management for the Wildlife Detection project",
+                },
+            ],
+            metadata: [
+                {
+                    key: "Occupation",
+                    value: "Senior Developer Relations Manager",
+                },
+                {
+                    key: "Years of Experience",
+                    value: "6",
+                },
+                {
+                    key: "Academic Degree",
+                    value: "B.Sc. (Hons) Computer Systems, Graduated with First Class Honors",
+                },
+            ]
+        },
+        {
+            username: "Mouzah.Qareiny",
+            name: "Mouzah Qareiny",
+            headline: "CodersHQ Ambassador",
+            position: "Software Developer, Ministry of Interior, Fujairah Police G.H.Q",
+            about: "I'm UAE youth women, with a bachelor's degree of science in Information Technology (Software Developer) from UAE University. I work on Fujairah Police G.H.Q as Full Stack Web Developer and administering various software project with their partners. I'm interested in learning a new programming language and exchange my knowledge with others, also I'm a proactive team worker with good communication skills and the ability of facing encountered problems.",
+            banner: "https://media-exp1.licdn.com/dms/image/D4D16AQHDj5vuPh2gvQ/profile-displaybackgroundimage-shrink_350_1400/0/1669522433706?e=1675296000&v=beta&t=smcvBhbQLP67qTL3vMZsyiK-A0rGp7Ne_PftxcoJTIw",
+            image: "https://media-exp1.licdn.com/dms/image/D4D03AQH-dzkkFxmbJA/profile-displayphoto-shrink_400_400/0/1669522013472?e=1675296000&v=beta&t=LF_ngjAE7aBUaBcWqxT7dtwnXeOy_8xPIzsN5RX-BP8",
+            highlights: [
+                "Bachelor's degree of science in Information Technology (Software Developer) from UAE University",
+                'CodersHQ Ambassador',
+                'Member of Arab Youth Technology Fellowship',
+                'His Highness the Minister of Interior Award-Technical Officer-Commander in Chief Award 2017(4th Session) and 2019(5th Session)',
+                'The best 250 participants in phase 1 and 2 at the level of the Arab World under the One million Arab programmers initiative'		
+            ],
+            socials: [
+                {
+                    name: "LinkedIn",
+                    url: "https://www.linkedin.com/in/mouzah-mosabbah/",
+                    icon: SlSocialLinkedin,
+                },
+                {
+                    name: "Github",
+                    url: "https://github.com/mouzah828",
+                    icon: SlSocialGithub,
+                },
+            ],
+            timeline: [
+                {
+                    date: "October 2022",
+                    title: "CodersHQ Gold Ambassador",
+                    description: "",
+                },
+                {
+                    date: "August 2013",
+                    title: "Joined Fujairah Police G.H.Q",
+                    description: "I built Web based applications with PHP and asp.net(C#) in addition to designing and implementing database using Oracle and MS SQL",
+                },
+            ],
+            metadata: [
+                {
+                    key: "Occupation",
+                    value: "Software Developer",
+                },
+                {
+                    key: "Years of Experience",
+                    value: "10",
+                },
+                {
+                    key: "Academic Degree",
+                    value: "Bachelor of science in Information Technology,Software Developer",
+                },
+            ]
+        }
     ],
 }
 
