f02c4ad49003713adaef6e45c55b547ddfc0befe
Husayn Gokal <husaynabbasgokal@gmail.com>
2022-11-27 22:52:46 +0400

    Added Profile + Fixed Spelling Error
    Changed "directorys" to "directories"
    
    Added Husayn Gokal's profile

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,7 +4,7 @@ import { data } from "../data";
 
 
 export default function CardList() {
-  const directoryList = data.directorys.map((data) => (
+  const directoryList = data.directories.map((data) => (
     <div key={data.username} className="my-5 md:my-0">
       <CardItem event={data}  />
     </div>
